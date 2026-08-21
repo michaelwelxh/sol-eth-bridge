217179849c4e7cb5458c18b6ea49acc0a61fc954
Michael Welch <michaelwelxh@gmail.com>
2026-08-21 19:34:05 +0100

    feat: add deposition nonce and safety

--- src/Bridge.sol
+++ src/Bridge.sol
@@ -5,6 +5,7 @@ import {IBridge} from "./interfaces/IBridge.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract Bridge is IBridge, Ownable, Pausable {
     // accept user token deposits 
@@ -14,35 +15,58 @@ contract Bridge is IBridge, Ownable, Pausable {
     // prevent replays
     // allows authorised messages to trigger the desitination side actions
     // support emergency pause
-    
+
+    // used to check the transfere succeded
+    using SafeERC20 for IERC20;
     IERC20 public immutable token;
+    uint256 public nonce;
+
 
     constructor(address _token) Ownable(msg.sender) {
         token = IERC20(_token);
     }
-    // recipt structure 
 
 
     function deposit(
-        address recipient, 
-        uint256 amount, 
+        address recipient,
+        uint256 amount,
         uint256 destinationChain
     ) external override whenNotPaused {
         // lock tokens
         require(amount > 0, "Amount must be greater then 0");
         require(recipient != address(0), "Invalid recipent");
 
-        // user needs to first use token.approve(address(this), amount);
-        token.transferFrom(
+        bytes32 messageId = keccak256(
+            abi.encode(
+                block.chainid,
+                address(this),
+                msg.sender,
+                recipient,
+                amount,
+                destinationChain,
+                nonce
+            )
+        );
+
+        nonce++;
+        
+        // 1. token.approve(address(this), amount);
+        // 2. bridge.deposit(100 ether, recipient); and this code runs 
+        token.safeTransferFrom(
             msg.sender, 
             address(this), 
             amount
         );
+        // 4. then to release from bridge 'token.transfer(recipient, amount);'
 
         // emit the event
-        function emitRecipt() public {
-            emit Recipt();
-        }
+        emit DepositCreated(
+            msg.sender,
+            recipient,
+            amount,
+            destinationChain,
+            messageId
+        );
     }
 
     function pause() external override onlyOwner {
--- src/interfaces/IBridge.sol
+++ src/interfaces/IBridge.sol
@@ -4,23 +4,21 @@ pragma solidity ^0.8.13;
 // add event and errors
 interface IBridge {
     function deposit(
-        address recipient, 
-        uint256 amount, 
+        address recipient,
+        uint256 amount,
         uint256 destinationChain
     ) external;
 
     function pause() external;
     function unpause() external;
     
-    struct Deposit {
-        address sender;
-        address recipient;
-        uint256 amount;
-        uint256 destinationChain;
-        bytes32 messageId;
-    }
+    event DepositCreated(
+        address indexed sender,
+        address indexed recipient,
+        uint256 amount,
+        uint256 destinationChain,
+        bytes32 messageId
+    );
     
-    // create messaaeg 
-    event Recipt();
 
 }
