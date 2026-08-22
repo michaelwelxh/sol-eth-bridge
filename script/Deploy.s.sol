47e89d6005c66b5bebe9cff75707ee9cfa934371
Michael Welch <michaelwelxh@gmail.com>
2026-08-22 23:48:30 +0100

    chore: add Deploy script file

---
+++ script/Deploy.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script} from "forge-std/Script.sol";
+
+contract CounterScript is Script {
+    // Deploy
+
+        // bridgeToken 
+        // bridge 
+        // configures permissions 
+        // set required addess/configuration
+}
